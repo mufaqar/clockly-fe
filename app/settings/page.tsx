c15e67b7b285243ad34050452720cf878b54b248
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2024-02-29 19:18:56 +0500

    Settings

---
+++ app/settings/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+import React from 'react'
+import { FaCheck } from 'react-icons/fa'
+import { solidColors } from '@/const/colors'
+
+export default function Settings() {
+    const [activeImage, setActiveImage] = React.useState();
+
+    return (
+        <div className='sm:p-8'>
+            <div className='bg-white shadow-md p-8 rounded-lg'>
+                <div className="flex justify-between item-center w-full ">
+                    <h2 className="text-base font-semibold leading-7 text-gray-900 mb-3">
+                        Settings
+                    </h2>
+                </div>
+                <div>
+                    <h2 className="text-base font-semibold leading-7 text-gray-900 mb-3">
+                        Choose Color
+                    </h2>
+                    <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 gap-2 md:gap-3 lg:gap-6">
+                        {solidColors.map(({ color, i }: any) => {
+                            return (
+                                <div key={i}
+                                    className="image-container cursor-pointer relative">
+                                    <div className={`px-10 py-10 solid rounded-xl ${activeImage === color?.colorCode && '_active-bg'}`} style={{ background: color?.colorCode }}>
+                                        <div className='absolute bg-[#3F9BFC] p-[2px] right-4 top-4 rounded-full text-white flex justify-center items-center flex-col'>
+                                            <FaCheck />
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ const/colors.jsx
@@ -0,0 +1,42 @@
+export const solidColors = [
+    {
+        id:1,
+        colorCode: '#F59E0B'
+    },
+    {
+        id:2,
+        colorCode: '#1AD844'
+    },
+    {
+        id:3,
+        colorCode: '#3F9BFC'
+    },
+    {
+        id:4,
+        colorCode: '#434CE6'
+    },
+    {
+        id:5,
+        colorCode: '#DC70F7'
+    },
+    {
+        id:6,
+        colorCode: '#379ADC'
+    },
+    {
+        id:7,
+        colorCode: '#FBBD1C'
+    },
+    {
+        id:8,
+        colorCode: '#EE649F'
+    },
+    {
+        id:9,
+        colorCode: '#BC7249'
+    },
+    {
+        id:10,
+        colorCode: '#635D60'
+    },
+]
